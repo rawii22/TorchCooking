06529d737a4193c4efc805a697542ab841a86e2d
Ricky <rjromanach@gmail.com>
2020-02-01 02:42:30 -0600

    Cooker/Ingredient mismatch bugfix
    When the torch was used on a large ingredient (in the inventory), it would crash. We had to ensure that the item being clicked on by the torch (the target) was the ingredient.

--- modmain.lua
+++ modmain.lua
@@ -26,19 +26,19 @@ end
 --add changes
 AddPrefabPostInit("torch", AllowToCook)
 
---loop allows all characters to access this string
-for k, v in pairs(GLOBAL.STRINGS.CHARACTERS) do
-	v.ACTIONFAIL.COOK.TOODENSE = "It simply won't cook."
-end
-
 --Because the ACTION.COOK.fn in actions.lua is weird with wether the 'invobject' or 'target' is the item being cooked vs the cooker, we compare them by checking their tags/components
 local COOKFN = GLOBAL.ACTIONS.COOK.fn
 --the above line will, like before, store the whole function(and it's looooong)
 
 --this will override the original function. It will perform it, but then check to see if the item can be cooked by the torch(also through the food's caloric value)
 GLOBAL.ACTIONS.COOK.fn = function(act)
+	if act.invobject.components.cooker then
+		local temp = act.invobject
+		act.invobject = act.target
+		act.target = temp
+	end
 	local cancook, str = COOKFN(act)
-	if cancook == false then
+	if not cancook then
 		if act.invobject:HasTag("lightcooker") and act.target.components.edible ~= nil and act.target.components.edible.hungervalue > TUNING.CALORIES_SMALL then
 			return false, "TOODENSE"
 		elseif act.target:HasTag("lightcooker") and act.invobject.components.edible ~= nil and act.invobject.components.edible.hungervalue > TUNING.CALORIES_SMALL then
@@ -50,3 +50,12 @@ GLOBAL.ACTIONS.COOK.fn = function(act)
 		return cancook, str
 	end
 end
+
+local function AddCharacterSpeech()
+	--loop allows all characters to access this string
+	for k, v in pairs(GLOBAL.STRINGS.CHARACTERS) do
+		v.ACTIONFAIL.COOK.TOODENSE = "It simply won't cook."
+	end
+end
+--add changes
+AddSimPostInit(AddCharacterSpeech)
