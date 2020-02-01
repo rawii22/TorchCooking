06817af0e418f58d7c56d842ae8baa6f60e766ca
Ricky <rjromanach@gmail.com>
2020-02-01 00:22:06 -0600

    Update modinfo & preview image
    There was a problem with the repository from our original .git folder so we cloned it and updated it here.

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
-name = "Torch+"
-description = "Ever wanted to cook with fire? Why can't you cook with your torch? Well now you can."
+name = "Torch+ (git)"
+description = "Ever wanted to cook with fire? Why can't you cook with your torch? Well now you can.\nIf you hover a food item that is considered small over the torch, it will cook it. e.g. seeds, berries, morsels, carrots."
 author = "rawii22 & lord_of_les_ralph"
 version = "1.0"
 
@@ -9,7 +9,7 @@ api_version = 10
 
 priority = - 1
 dst_compatible = true
-all_clients_require_mod = false
+all_clients_require_mod = true
 client_only_mod = false
 
 configuration_options =
