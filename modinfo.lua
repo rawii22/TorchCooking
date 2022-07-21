466313c6d904f5b45c41650fabe93494b1c077f6
Ricky <rjromanach@gmail.com>
2022-07-21 04:20:41 -0500

    Fuel size configurable
    The items allowed to fuel a torch (based on its fuelvalue) can be configured. The option to "Add Fuel" will not be visible for items that are too large.

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "Torch+ (git)"
-description = "Ever wanted to cook with fire? Why can't you cook with your torch? Well now you can.\nIf you hover a food item over the torch, it will cook it.\nTorches can also be fueled by anything that can fuel a fire pit."
+description = "Ever wanted to cook with fire? Why can't you cook with your torch? Well now you can.\nIf you hover a food item over the torch, it will cook it.\nTorches can also be fueled by burnable items. The size of these items can be configured."
 author = "rawii22 & lord_of_les_ralph"
-version = "1.3"
+version = "1.4"
 icon = "modicon.tex"
 icon_atlas = "modicon.xml"
 
@@ -16,13 +16,26 @@ client_only_mod = false
 
 configuration_options = {
 	{
-		name = "FUELABLE",
+		name = "TORCHFUELABLE",
 		label = "Fuelable torches",
-		hover = "This allows you to fuel torches with burnable items.",
+		hover = "Allows you to fuel torches with burnable items.",
 		default = true,
 		options = {
 			{description = "Enabled", data = true},
 			{description = "Disabled", data = false}
+		}
+	},
+	{
+		name = "KINDLINGFUELLIMIT",
+		label = "Fuel item size",
+		hover = "Set the largest burnable item that can fuel a torch.",
+		default = "MED_FUEL",
+		options = {
+			{description = "Tiny fuel", data = "TINY_FUEL"},
+			{description = "Small fuel", data = "SMALL_FUEL"},
+			{description = "Medium fuel", data = "MED_FUEL"},
+			{description = "Medium-large fuel", data = "MED_LARGE_FUEL"},
+			{description = "Large fuel", data = "LARGE_FUEL"}
 		}
 	},
 }
--- modmain.lua
+++ modmain.lua
@@ -1,18 +1,50 @@
-local FUELABLE = GetModConfigData("FUELABLE")
+local TORCHFUELABLE = GetModConfigData("TORCHFUELABLE")
+KINDLINGFUELLIMIT = GLOBAL.TUNING[GetModConfigData("KINDLINGFUELLIMIT")]
+
+-- Add new fuel type
+GLOBAL.FUELTYPE.KINDLING = "KINDLING"
 
 local function ontakefuel(inst)
     inst.SoundEmitter:PlaySound("dontstarve/common/fireAddFuel")
 end
 
---will give the torch prefab the ability to cook (with tag used above) --and fuelable too (7/18/2022)
+-- Will give the torch prefab the ability to cook, and to accept fuel too (7/18/2022)
 local function ModifyTorch(prefab)
 	prefab:AddComponent("cooker")
-	if FUELABLE then
+	if TORCHFUELABLE then
 		if prefab.components.fueled ~= nil then
 			prefab.components.fueled.accepting = true
 			prefab.components.fueled:SetTakeFuelFn(ontakefuel)
 		end
 	end
 end
---add changes
 AddPrefabPostInit("torch", ModifyTorch)
+
+--this function prevents the "add fuel" option from appearing on burnable items that are too large and acting on a torch
+local function OverrideCollectActions(prefab)
+	local fuel = prefab.components.fuel or prefab.components.fueler
+	if fuel and fuel.fueltype == GLOBAL.FUELTYPE.BURNABLE and fuel.fuelvalue > KINDLINGFUELLIMIT then --check whether the prefab has fuel or fueler component and if it has the right fuel type and value
+		local OldCollectActions = prefab.CollectActions
+		prefab.CollectActions = function(self, actiontype, ...)
+			OldCollectActions(self, actiontype, ...) --CollectActions from componentactions compiles a list of actions that an item can perform based on the components of that item
+			if actiontype == "USEITEM" then --fueling a torch is a "USEITEM" type of action
+				local _, target, actions = ... --"USEITEM" actions use these specific parameters. (Look at componentactions.lua)
+				if target.prefab == "torch" then
+					local fuelActionIdx = nil
+					for i,action in ipairs(actions) do --find the fuel action
+						if action.id == "ADDFUEL" or action.id == "ADDWETFUEL" then
+							fuelActionIdx = i
+						end
+					end
+					if fuelActionIdx then --if it has a fuel action, remove it
+						table.remove(actions, fuelActionIdx)
+					end
+				end
+			end
+		end
+	end
+end
+
+if TORCHFUELABLE then
+	AddPrefabPostInitAny(OverrideCollectActions)
+end
